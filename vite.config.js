790bddf96ea4f415d92cf748666957bb84bd746e
Chris <chriscilot9@gmail.com>
2026-02-25 19:12:01 -0700

    Allow dev Host Replit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: [
+      '8aa5517f-32fa-4544-8d77-cf52d7282a0e-00-i6pkwo9osn90.riker.replit.dev'
+    ]
+  }
 })
